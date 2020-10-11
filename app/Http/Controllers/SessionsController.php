27bc93a9a77e9985492323dc619597ff088c0e77
vagrant <vagrant@homestead>
2020-10-11 09:50:16 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 use Auth;
 
 class SessionsController extends Controller
@@ -11,6 +11,10 @@ class SessionsController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
         $this->middleware('guest', [
             'only' => ['create']
         ]);
@@ -24,20 +28,20 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-        $credentials = $this->validate($request, [
-            'email' => 'required|email|max:255',
-            'password' => 'required'
-        ]);
- 
-        if (Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
-        } else {
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
-     }
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
+    }
 
     public function destroy()
     {

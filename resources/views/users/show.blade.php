1b0e2b21e8d24870293cae2ab76db19b7700568d
vagrant <vagrant@homestead>
2020-10-12 10:10:10 +0000

    社交統計訊息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,17 +2,15 @@
 @section('title', $user->name)
 
 @section('content')
-<div class="row ">
+<div class="row">
   <div class="offset-md-2 col-md-8">
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
     </section>
-    @if(Auth::check() && $user->id == Auth::id())
-    <section class="status_form">
-        @include('shared._status_form')
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => $user])
     </section>
-@endif
-<hr>
+    <hr>
     <section class="status">
       @if ($statuses->count() > 0)
         <ul class="list-unstyled">

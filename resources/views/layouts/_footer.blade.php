7cb682a39d8186dc460d88e568f16105b3778557
vagrant <vagrant@homestead>
2020-10-24 01:35:44 +0000

    修改BUG

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer mb-3">
   <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-  <a href="https://learnku.com/laravel/courses" target=_blank>
+  <a href="#" target=_blank>
     持續練習，每日精進
   </a>
 
